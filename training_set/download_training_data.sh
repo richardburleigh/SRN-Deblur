3b82a92936f341c5390b4b9e90075f2742a56a8c
Do Thanh Luan <luandt@relipasoft.com>
2019-05-09 08:31:09 +0700

    update data to training

---
+++ training_set/download_training_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./download_drive.sh "1H0PIXvJH4c40pk7ou6nAwoxuR4Qh_Sa2" "2103.zip" && unzip "2103.zip" && mv train/* ./
+
+./download_drive.sh "1iUiqUu9P_TAb4oC6HUG08CU8D_i6zgAg" "297.zip" && unzip "297.zip"
+
